62cd1139019d9d4b08b3e994202f75ea714884c8
Mike Hawes <mhawes24@gmail.com>
2019-10-23 21:51:38 -0400

    Cleaning up views.py and addd serializer for incoming and outgoing data,

--- api/analyze/views.py
+++ api/analyze/views.py
@@ -1,45 +1,47 @@
+# [Django]
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+
+# [App]
+from api.analyze.serializers import UrlQueryParamSerizlizer, AnalysisSerializer
 from api.analyze.utils import (
-    get_privacy_policy_url,
-    get_site_html,
+    find_privacy_policy_url,
+    download_html,
     text_from_html,
     get_fuzzy_result,
 )
-from rest_framework.response import Response
 
 
 class AnalyzeUrlView(APIView):
     permission_classes = ()
     authentication_classes = ()
-    serializer_class = None
+    serializer_class = AnalysisSerializer
 
     def get(self, request, *args, **kwargs):
-        url = request.GET.get("url")
-        pp_url = get_privacy_policy_url(url)
+        """
+        Find the privacy policy from the given url. Classify on corpus data to
+        analyze the document and parse out the main points in correspondence to the
+        actions ("access", "store", "sell", "disclose", "delete").
 
-        if not pp_url:
-            return Response("Privacy Policy not found.", status=404)
+        :return: Response<JSON>
+        """
+        params = UrlQueryParamSerizlizer(data=request.GET)
+        if not params.is_valid():
+            return Response(params.errors, status=HTTP_400_BAD_REQUEST)
 
-        # Download all text from PP
-        html = get_site_html(pp_url)
-        privacy_policy_text = text_from_html(body=html)
+        # Locate privacy policy url
+        privacy_policy_url = find_privacy_policy_url(params.data.get("url"))
 
+        # Download policy html & parse out text
+        html = download_html(privacy_policy_url)
+        text = text_from_html(body=html)
+
+        # For each action, create an action dict --> ActionSerializer
         actions = ("access", "store", "sell", "disclose", "delete")
-        all_actions = []
-        for action in actions:
-            result = get_fuzzy_result(action, text=privacy_policy_text)
-            action_map = dict(
-                type=action.title(),
-                quote=result[0][0],
-                confidence=result[0][1],
-                classification=result[0][2],
-            )
-            all_actions.append(action_map)
-
-        return Response({"actions": all_actions, "privacy_policy_link": pp_url})
-
-    """
-    Actions are delete store, etc
-    results is a list of tuples
-    (quote, classifier_result)
-    """
+        return_data = dict(
+            actions=[get_fuzzy_result(action, text=text) for action in actions],
+            privacy_policy_link=privacy_policy_url,
+        )
+        serializer = self.serializer_class(return_data)
+        return Response(serializer.data, status=HTTP_200_OK)
